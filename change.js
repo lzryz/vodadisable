4a6ba362a76f6a477ec5bed7f14c509102b9dba7
lzryz <78732957+lzryz@users.noreply.github.com>
2024-06-26 11:16:24 +0200

    Create change.js

---
+++ change.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Vodafone
+// @version      1.0
+// @description  :)
+// @author       lazar
+// @match        http://192.168.0.1/*
+// @icon         https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Vodafone_icon.svg/765px-Vodafone_icon.svg.png
+// @updateURL    https://github.com/lzryz/vodadisable/change.js
+// @downloadURL  https://github.com/lzryz/vodadisable/change.js
+// @grant        none
+// ==/UserScript==
+
+(function Change() {
+    document.getElementById('c_42').disabled = false;
+})();
